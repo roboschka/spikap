19d54c23d7eaa14fb0fc1bf78f653d8376b17ceb
Aries <37924647+ariesdwi@users.noreply.github.com>
2020-07-08 10:47:35 +0700

    Xcode Spikap

---
+++ Spikap/Spikap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spikap
+//
+//  Created by Aries Dwi Prasetiyo on 08/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
