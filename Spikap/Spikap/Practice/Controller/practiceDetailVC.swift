aa1243c7393e94cc272ba94cdc0357c2956f4f53
Muhammad Rivan Rayhan <rayhanrivan@gmail.com>
2020-07-21 11:56:21 +0700

    Add assets and practiceDetailVC

---
+++ Spikap/Spikap/Practice/Controller/practiceDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  practiceDetailVC.swift
+//  Spikap
+//
+//  Created by Muhammad Rivan Rayhan on 21/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class practiceDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
