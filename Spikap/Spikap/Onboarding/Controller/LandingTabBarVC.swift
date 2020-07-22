40ce8d3f8dba9b4f86df356edd955f8220543f68
ariesdwi <37924647+ariesdwi@users.noreply.github.com>
2020-07-22 09:54:50 +0700

    Onboarding fix  (#18)
    * initial user state change
    
    * setup iCloud container - cloudKit
    
    * add post container to CloudKit
    
    * setup iCloud container - cloudKit
    
    * create user record on CloudKit
    
    * UIKit extension + setting up custom home navBar
    
    * Nambah view di onboarding
    
    * Button apple dan button guest
    
    * Sign In With Apple
    
    * Onboarding
    
    * Pembaruan
    
    * Onboard change to LandingTabbar an fix stpry board onboarding
    
    * Onboarding UserDefault Fix
    
    Co-authored-by: Maria Jeffina <mariajeffina@gmail.com>

---
+++ Spikap/Spikap/Onboarding/Controller/LandingTabBarVC.swift
@@ -0,0 +1,56 @@
+//
+//  onboardVC.swift
+//  Spikap
+//
+//  Created by Aries Dwi Prasetiyo on 10/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class LandingTabBarVC: UITabBarController {
+    var currentUser = [User]()
+    let currentGuest = GuestModel()
+    var showOnboarding = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showOnboarding = UserDefaults.standard.bool(forKey: "showOnboarding")
+    }
+    enum ViewControllerType {
+        case welcome
+        case home
+    }
+    //MARK: Check if user is signed in or not
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if showOnboarding == true {
+            pushTo(viewController: .home)
+        }else {
+            //            pushTo(viewController: .home)
+            pushTo(viewController: .welcome)
+            showOnboarding =  true
+            UserDefaults.standard.set(showOnboarding, forKey: "showOnboarding")
+        }
+    }
+    
+    //MARK: Push to relevant ViewController
+    func pushTo(viewController: ViewControllerType)  {
+        switch viewController {
+        case .home:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "LandingTabBarVC") as! UITabBarController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
+        case .welcome:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "loginViewController") as! LoginViewController
+            vc.modalPresentationStyle = .fullScreen
+            //vc.isModalInPresentation = true
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
+
+
+
+
