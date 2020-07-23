9611ef00c01ebbdd0af1dd3b45dd8593038a80fa
Jeffina Wu <47268934+jeffinawu@users.noreply.github.com>
2020-07-23 22:11:26 +0700

    AVFoundation, Audio Engine, Playback enabling flag (#24)
    * initial user state change
    
    * setup iCloud container - cloudKit
    
    * add post container to CloudKit
    
    * setup iCloud container - cloudKit
    
    * create user record on CloudKit
    
    * UIKit extension + setting up custom home navBar
    
    * Profile view + days on streak
    
    * fix font
    
    * update homeVC
    
    * add gitignore + change color
    
    * finish home
    
    - guest data model
    - guest points and manage level name/limit
    - daysOnStreak, isOnStreak (dummy)
    
    * revert gitignore
    
    * fix changes
    
    * delete DS_Store
    
    * segue to profile, fix progress bar, fix day streak logic
    
    * fix landingTabBar
    
    * added font changes, auto layout in practice page
    
    * Practice speech card + add dummy data
    
    * congratulations view + segue back to practice & to congrats view
    
    * Revert "Merge branch 'master' into jeff"
    
    This reverts commit 149ac21f1c677985f6312eb24f5ed50d534e7308, reversing
    changes made to 2f8632fa12a82e438c816409230b33403adcff05.
    
    * fix audio engine, play button enabling flag
    
    * change isRecording true button

--- Spikap/Spikap/Practice/Controller/SpeechShadowingViewController.swift
+++ Spikap/Spikap/Practice/Controller/SpeechShadowingViewController.swift
@@ -7,48 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SpeechShadowingViewController: UIViewController {
+class SpeechShadowingViewController: UIViewController, AVAudioRecorderDelegate {
     //MARK: Variables
     var topic = "Travelling"
     var totalProgress = 8
-    var currentProgress = 2
+    var currentProgress = 0
     
-    var contents = "Travel"
-    var contentsToken = ["Tra", "vel"]
-    var info = ["This is the definition of the word"]
+    var contents = ["Travel", "Backpack", "Map", "Roadtrip", "Wanderlust", "Van", "Recreation", "Compass"]
+    var contentsToken = [["Tra", "vel"], ["Back", "pack"], ["Map"], ["Road", "trip"], ["Wan", "der", "lust"], ["Van"], ["Re", "crea", "tion"], ["Com", "pass"]]
+    var info = ["This is the definition of the word. Please do write some long sentences just to test out auto layout"]
     var result = true
     
+    let audioEngine = AVAudioEngine()
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var inputFormat: AVAudioFormat!
+    var audioFileName: URL!
+    
+    let player = AVQueuePlayer()
+    
+    var isRecording: Bool = false
     
     //MARK: IB Outlet
     @IBOutlet weak var topicLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var tokenLabel: UILabel!
+    @IBOutlet weak var contentInfoLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var progressBarView: UICollectionView!
+    @IBOutlet weak var playAudioButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
         setupNextButton()
+        setupTokenLabel(progress: currentProgress)
+        recordPermission()
+        playAudioButton.isEnabled = true
     }
-    
     
     override func viewWillAppear(_ animated: Bool) {
         changeToSystemFont(label: topicLabel, fontSize: 20)
         
         topicLabel.text = topic
-        questionLabel.text = contents
-        for index in 0 ..< contentsToken.count {
-            let value = contentsToken[index]
-            if index == contentsToken.count - 1 {
-                tokenLabel.text = tokenLabel.text ?? "" + value
-            } else {
-                tokenLabel.text = tokenLabel.text ?? "" + value + " - "
-            }
-            
-        }
+        questionLabel.text = contents[0]
+        
+        contentInfoLabel.text = info[0]
     }
     
+    //MARK: IB Actions
     @IBAction func goBack(_ sender: Any) {
         let alert = UIAlertController(title: "Are you sure you want to go back?", message: "You will loose your progress by going back", preferredStyle: .alert)
         
@@ -61,15 +72,122 @@ class SpeechShadowingViewController: UIViewController {
         }))
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
         self.present(alert, animated: true)
+    }
+    
+    
+    @IBAction func nextProgressButton(_ sender: Any) {
+        currentProgress += 1
+        if(currentProgress > totalProgress - 1) {
+            performSegue(withIdentifier: "toCongratulations", sender: nil)
+        } else {
+            questionLabel.text = contents[currentProgress]
+            setupTokenLabel(progress: currentProgress)
+            progressBarView.reloadData()
+        }
+    }
+    
+    //MARK: Functions
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadRecordingUI() {
+        print("Tap to record")
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+    }
+    
+    @objc func recordTapped() {
+        if audioRecorder == nil {
+            startRecording()
+            playAudioButton.isEnabled = false
+        } else {
+            finishRecording(success: true)
+            playAudioButton.isEnabled = true
+        }
+    }
+    
+    func startRecording() {
+        audioFileName = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        recordButton.setImage(#imageLiteral(resourceName: "record button"), for: .normal)
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFileName, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            do {
+                try audioEngine.start()
+            } catch ( _) {
+                print("error in starting audio engine")
+            }
+            print("Tap to stop")
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    
+    func finishRecording(success: Bool) {
+        recordButton.setImage(#imageLiteral(resourceName: "mic button"), for: .normal)
+        audioEngine.stop()
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            print("tap to re-record")
+            print(audioFileName.absoluteString)
+        } else {
+            print("tap to record")
+        }
+    }
+    
+    func recordPermission(){
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true, options: [])
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
     }
     
     func setupNextButton(){
         nextButton.imageView?.contentMode = .scaleAspectFit
         nextButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: -5)
+        nextButton.layer.isHidden = false
     }
-
+    
+    func setupTokenLabel(progress: Int){
+        var finalString: String = ""
+        for index in 0 ..< contentsToken[progress].count {
+            let value = contentsToken[progress][index]
+           
+            if index == contentsToken[progress].count - 1 {
+                finalString = finalString + value
+            } else {
+                finalString = finalString + value + " - "
+            }
+        }
+        tokenLabel.text = finalString
+    }
 }
 
 extension SpeechShadowingViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
@@ -103,7 +221,9 @@ extension SpeechShadowingViewController: UICollectionViewDataSource, UICollectio
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PracticeProgressCell", for: indexPath) as! PracticeProgressCollectionViewCell
         cell.practiceBar.layer.cornerRadius = 0.1 * cell.practiceBar.bounds.height
         
-        if (indexPath.row > (currentProgress-1)) {
+        if (indexPath.row <= (currentProgress-1)) {
+            cell.practiceBar.layer.backgroundColor = #colorLiteral(red: 0.3921568627, green: 0.8235294118, blue: 1, alpha: 1)
+        } else {
             cell.practiceBar.layer.backgroundColor = #colorLiteral(red: 0.8823529412, green: 0.8823529412, blue: 0.8823529412, alpha: 1)
         }
         
