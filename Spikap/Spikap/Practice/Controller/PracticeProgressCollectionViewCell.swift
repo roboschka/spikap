d491bc7fe09d8b0af0c116b443d77271f7dd4b9d
Jeffina Wu <47268934+jeffinawu@users.noreply.github.com>
2020-07-23 09:40:55 +0700

    font + auto layout practice screen (#22)
    * initial user state change
    
    * setup iCloud container - cloudKit
    
    * add post container to CloudKit
    
    * setup iCloud container - cloudKit
    
    * create user record on CloudKit
    
    * UIKit extension + setting up custom home navBar
    
    * Profile view + days on streak
    
    * fix font
    
    * update homeVC
    
    * add gitignore + change color
    
    * finish home
    
    - guest data model
    - guest points and manage level name/limit
    - daysOnStreak, isOnStreak (dummy)
    
    * revert gitignore
    
    * fix changes
    
    * delete DS_Store
    
    * segue to profile, fix progress bar, fix day streak logic
    
    * fix landingTabBar
    
    * added font changes, auto layout in practice page
    
    * Practice speech card + add dummy data

---
+++ Spikap/Spikap/Practice/Controller/PracticeProgressCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PracticeProgressCollectionViewCell.swift
+//  Spikap
+//
+//  Created by Maria Jeffina on 22/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class PracticeProgressCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var practiceBar: UIView!
+    
+}
