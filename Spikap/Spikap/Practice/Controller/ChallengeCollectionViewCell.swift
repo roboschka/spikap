34ec11eff3555eab5a96f20f366e05c883f2f621
Muhammad Rivan Rayhan <rayhanrivan@gmail.com>
2020-07-24 08:50:02 +0700

    Add assets & add ChallengeOverviewVC

---
+++ Spikap/Spikap/Practice/Controller/ChallengeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeCollectionViewCell.swift
+//  Spikap
+//
+//  Created by Muhammad Rivan Rayhan on 24/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
