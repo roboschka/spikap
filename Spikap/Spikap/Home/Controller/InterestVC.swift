bd09250747228fb56dfa8c5db0e5b73e8cf910ee
Grace Cindy <megumi.cindy@gmail.com>
2020-07-28 07:05:05 +0700

    added interest list

---
+++ Spikap/Spikap/Home/Controller/InterestVC.swift
@@ -0,0 +1,97 @@
+//
+//  InterestVC.swift
+//  Spikap
+//
+//  Created by Grace Cindy on 27/07/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class InterestVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //change model by Interest DB here
+    struct InterestModel
+    {
+        var interestID : Int
+        var interestName: String
+        var selected: Bool = false
+    }
+    
+    //dummy Interest
+    let interestList = [
+        InterestModel(interestID: 1, interestName: "Animal", selected: false),
+        InterestModel(interestID: 2, interestName: "Art & Design", selected: false),
+        InterestModel(interestID: 3, interestName: "Cartoon", selected: false),
+        InterestModel(interestID: 4, interestName: "Game", selected: false),
+        InterestModel(interestID: 5, interestName: "Movies", selected: false),
+        InterestModel(interestID: 6, interestName: "Music", selected: false),
+        InterestModel(interestID: 7, interestName: "Sports", selected: false),
+        InterestModel(interestID: 8, interestName: "Travelling", selected: false)
+    ]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return interestList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "interestCell", for: indexPath)
+        
+        let item = interestList[indexPath.row]
+        
+        cell.textLabel?.text = item.interestName
+        cell.accessoryType = item.selected ? .checkmark : .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+            
+            interestList[indexPath.row].selected = !interestList[indexPath.row].selected
+            
+            if interestList[indexPath.row].selected {
+                if !repeatDayIndex.contains(indexPath.row){
+                    repeatDayIndex.append(indexPath.row)
+                }
+                else {
+                    repeatDayIndex = repeatDayIndex.filter{$0 != indexPath.row}
+                }
+                
+            } else {
+                if repeatDayIndex.count != 0 {
+    //                repeatDay.remove(at: indexPath.row)
+                    repeatDayIndex = repeatDayIndex.filter{$0 != indexPath.row}
+                }
+                
+            }
+            
+            print("repeat day \(repeatDayIndex)")
+            
+            tableView.reloadData()
+            
+        }
+    
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
