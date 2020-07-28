a256b520b2409001baefb263ec17a59d0fceba43
Aries <37924647+ariesdwi@users.noreply.github.com>
2020-07-28 10:01:38 +0700

    Create and Get Data Done

--- Spikap/Spikap/Onboarding/Controller/LandingTabBarVC.swift
+++ Spikap/Spikap/Onboarding/Controller/LandingTabBarVC.swift
@@ -10,7 +10,8 @@ import UIKit
 import AuthenticationServices
 
 class LandingTabBarVC: UITabBarController {
-    var currentUser = [User]()
+//    var currentUser = [User]()
+    var datamodel = userModel()
     let currentGuest = GuestModel()
     var showOnboarding = true
     
@@ -25,16 +26,41 @@ class LandingTabBarVC: UITabBarController {
     //MARK: Check if user is signed in or not
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if showOnboarding == true {
-
-//            pushTo(viewController: .home)
-
-        }else {
-            //            pushTo(viewController: .home)
-            pushTo(viewController: .welcome)
-            showOnboarding =  true
-            UserDefaults.standard.set(showOnboarding, forKey: "showOnboarding")
+        
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier ?? "") { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                //MARK: - CloudKit
+                Userextention.getMemberBySpecificEmail(email: "ariesdwiprasetiyo4@gmail.com", successCompletion: { (datamodel) in
+                    self.datamodel = datamodel
+                    print("\n\nPoint = \(datamodel.userPoints)\n\nUserEmail = \(datamodel.userEmail)\n\nUserFullName = \(datamodel.fullname)\n\n")
+                }){
+                    (message)in
+                    print(message)
+                }
+                
+            break // The Apple ID credential is valid.
+            case .revoked, .notFound:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.6) {
+                    self.pushTo(viewController: .welcome)
+                }
+            default:
+                break
+            }
         }
+        //        if showOnboarding == true {
+        //
+        ////            pushTo(viewController: .home)
+        //
+        //        }else {
+        //            //            pushTo(viewController: .home)
+        //            pushTo(viewController: .welcome)
+        //            showOnboarding =  true
+        //            UserDefaults.standard.set(showOnboarding, forKey: "showOnboarding")
+        //        }
     }
     
     //MARK: Push to relevant ViewController
