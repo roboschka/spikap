b2e45db9ce1201fc6c10872efd61491fd803134f
rivan184 <48507703+rivan184@users.noreply.github.com>
2020-08-05 09:52:36 +0700

    Rivan (#50)
    * initial user state change
    
    * setup iCloud container - cloudKit
    
    * Add Navbar
    
    * Add Navbar
    
    * add Community & Practice navbar
    
    * Set up practice card tableview
    
    * Add assets and practiceDetailVC
    
    * Add segue to practiceDetailVC
    
    * add PracticeDetailTableViewCell and configure the TableViewCell
    
    * import Assets
    
    * fix Navigationbar fontsize
    
    * check prefers Large Title
    
    * Add ActivitiesTableView
    
    * Add assets & add ChallengeOverviewVC
    
    * add Collection View on ChallengeOverviewVC
    
    * Fix tab bar
    
    * Fix tab bar and nav bar title
    
    * change device view
    
    * Add SelfTalkViewController
    
    * add segue to SelfTalk
    
    * Add chat function
    
    * Set up record button and speech to text
    
    * Add speech to text to SelfTalk
    
    * Add segue from SelfTalkVC to CongratulationsVC
    
    * add pronounciation check to SelfTalk
    
    * Add ChallengeOverview description dummy data
    
    * Add segue from Challenge to SelfTalk and SpeechOverview
    
    * Configure ChallengeOverview CollectionViewCell color
    
    Co-authored-by: Maria Jeffina <mariajeffina@gmail.com>

--- Spikap/Spikap/Practice/Controller/ChallengeOverviewViewController.swift
+++ Spikap/Spikap/Practice/Controller/ChallengeOverviewViewController.swift
@@ -21,7 +21,8 @@ class ChallengeOverviewViewController: UIViewController {
     @IBOutlet weak var practiceLevelLabel: UILabel!
     
     var practiceId:Int?
-    var challengeDesc:[String] = ["Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Go to community and post any topic you like","Go to community and comment if some of the post","Go to community and post any topic you like"]
+    var challengeDesc:[String] = ["Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your pronunciation using Vacation topic in speech shadowing","Practice your pronunciation using Sport topic in speech shadowing","Practice your pronunciation using Culture topic in speech shadowing","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Practice your speaking skills using Travelling topic in self-talk","Practice your speaking skills using Job Interview topic in self-talk","Practice your speaking skills using Ordering food topic in self-talk","Go to community and post any topic you like","Go to community and post any topic you like","Go to community and post any topic you like"]
+    var forDay:Int = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +36,12 @@ class ChallengeOverviewViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.collectionView.selectItem(at: IndexPath.init(item: forDay, section: 0), animated: true, scrollPosition: [])
+        dayLabel.text = "Day \(forDay + 1)"
+        dayDescriptionLabel.text = challengeDesc[forDay]
     }
     
     func loadData(){
@@ -56,6 +63,19 @@ class ChallengeOverviewViewController: UIViewController {
         }
         dayDescriptionLabel.text = "Practice your pronunciation using Vacation topic in speech shadowing"
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    @IBAction func startTapped(_ sender: Any) {
+        switch forDay {
+        case 0...8:
+            performSegue(withIdentifier: "segueChallengeToSpeech", sender: nil)
+        case 9...17:
+            performSegue(withIdentifier: "segueChallengeToSelfTalk", sender: nil)
+        default:
+            break
+        }
+    }
 }
 
 extension ChallengeOverviewViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -66,8 +86,16 @@ extension ChallengeOverviewViewController: UICollectionViewDelegate, UICollectio
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "challengeCollectionViewCell", for: indexPath) as! ChallengeCollectionViewCell        
         
-        cell.daysLabel.text = "\(indexPath.row + 1)"
+        if forDay > indexPath.row {
+            cell.daysView.backgroundColor = UIColor(red: 255/255, green: 157/255, blue: 80/255, alpha: 1)
+            cell.daysLabel.textColor = UIColor.white
+        }else {
+            cell.daysView.backgroundColor = UIColor(red: 231/255, green: 231/255, blue: 231/255, alpha: 1)
+            cell.daysLabel.textColor = UIColor(red: 138/255, green: 138/255, blue: 138/255, alpha: 1)
+        }
         
+        
+        cell.daysLabel.text = "\(indexPath.row + 1)"
         return cell
     }
     
@@ -75,6 +103,7 @@ extension ChallengeOverviewViewController: UICollectionViewDelegate, UICollectio
         dayLabel.text = "Day \(indexPath.row + 1)"
         dayDescriptionLabel.text = challengeDesc[indexPath.row]
     }
+    
     
 }
 
